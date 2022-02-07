8c806a99cfe154c440fad92e3e6a1e7d5d4b22ab
NattapongSiri <s.nattapong@gmail.com>
2022-02-07 15:53:40 +0700

    Process csv file on client side

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import papa from 'papaparse'
 import './App.css';
+import { useCallback, useState } from 'react';
 
 function App() {
+  let [file, setFile] = useState()
+  let [content, setContent] = useState()
+  const showTableHandler = useCallback(() => {
+    let reader = new FileReader()
+    reader.readAsText(file)
+    reader.onload = e => {
+      let csv = e.target.result
+      setContent(
+        papa.parse(csv, {
+          header: true
+        }).data
+      )
+    }
+  }, [file, setContent])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" accept="text/csv" onChange={({target}) => setFile(target.files[0])}/>
+      <input type="button" onClick={showTableHandler} value="show table"/>
+      <table>
+        <thead>
+          <tr>{content?.length > 0 && Object.keys(content[0]).map(name => (
+            <td key={name}>{name}</td>
+          ))
+          }</tr>
+        </thead>
+        <tbody>
+          {content?.length > 0 && content.map((row, index) => {
+            return <tr key={index}>{Object.values(row).map((v, index) => (
+              <td key={index}>{v}</td>
+            ))}</tr>
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
